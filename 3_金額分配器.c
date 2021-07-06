728a5396082ff8f8343a74fbebc33c63b8892d6f
flyingdollar <flyingdollar.cheng@gmail.com>
2021-07-06 13:31:37 +0800

    trash

--- 3_金額分配器.c
+++ 3_金額分配器.c
@@ -7,7 +7,7 @@ int main()
     printf("要將多少錢平分給3個人呢？\n");
     printf("請輸入差距的金額(正負皆可)：");
     scanf("%d", &money);
-    remainder = money % 3;
+    remainder = money % 30;
     remainder = (remainder + 3) % 3;
     quotient = (money - remainder) / 3;
     printf("每個人 %d 元，剩下 %d 元\n", quotient, remainder);
