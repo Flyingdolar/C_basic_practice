457a99ab9d4d5e56e5e869aea445551da0e49282
Flyingdollar <flyingdollar.cheng@gmail.com>
2021-09-04 16:46:50 +0800

    hw15update

---
+++ 15_求質因數.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+    int n, target, total;
+    int count = 0;
+    printf("請輸入測試次數：");
+    scanf("%d",&n);
+
+    while(n--){
+        printf("目標數字：");
+        scanf("%d",&target);
+        total = 0;
+
+        for (int i = 1; i < target; i++)
+        {
+            if (target % i == 0){
+
+                for (int j = 1; j <= i; j++){
+                    count = i % j == 0 ? count+1 : count;
+                    if(count > 2){
+                        break;
+                    }
+                }
+                if(count == 2){
+                    printf("%d ",i);
+                    total ++;
+                }
+                count = 0;
+            }
+        }
+        if(total == 0){
+            printf("%d ",target);
+            total++;
+        }
+
+        printf("總共有 %d 個\n",total);
+    }
+}
